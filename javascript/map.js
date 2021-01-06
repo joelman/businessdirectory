8074cb54ae87bcd4150e7bd1a7cec4aa0133ea9a
Joel Gwynn <joelman@gmail.com>
2021-01-06 14:06:26 -0500

    size tweak

--- javascript/map.js
+++ javascript/map.js
@@ -3,12 +3,12 @@
 var mymap = L.map('mapid').setView([42.247, -72], 8);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-maxZoom: 18,
-id: 'mapbox/streets-v11',
-tileSize: 512,
-zoomOffset: -1,
-accessToken: 'pk.eyJ1Ijoiam9lbG1hbiIsImEiOiJja2praTZrc2MyNTR3MnVwODg2ZDg2MDBlIn0.lCStPSDt-yVoHjHSl9adUQ'
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoiam9lbG1hbiIsImEiOiJja2praTZrc2MyNTR3MnVwODg2ZDg2MDBlIn0.lCStPSDt-yVoHjHSl9adUQ'
 }).addTo(mymap);
 
 var neighborhoods = ['Boston', 'Allston', 'Back Bay', 'Bay Village', 'Beacon Hill', 'Brighton', 'Charlestown', 'Chinatown–Leather District', 'Dorchester', 'Downtown', 'East Boston', 'Fenway-Kenmore (includes Longwood)', 'Hyde Park', 'Jamaica Plain', 'Mattapan', 'Mission Hill', 'North End', 'Roslindale', 'Roxbury', 'South Boston', 'South End', 'West End', 'West Roxbury', 'Wharf District']
@@ -25,69 +25,71 @@ var maxLat = 0;
 var minLon = 0;
 var maxLon = -100;	  
 
-var corner1 = L.latLng(41.43536151749563, -73.74355142136532),
-corner2 = L.latLng(42.907075756871656, -70.0461509660558),
-maxBounds = L.latLngBounds(corner1, corner2);
+var corner1 = L.latLng(41, -74),
+    corner2 = L.latLng(43, -70),
+    maxBounds = L.latLngBounds(corner1, corner2);
 
 for(i = 0; i < length; i++) {
-  var row = businesses[i].split('\t');
-  [lat, lon, name, locality] = row;
-  
-  lat = parseFloat(lat)
-  lon = parseFloat(lon)
-  
-  // errors
-  if(!maxBounds.contains([lat, lon]))
+    var row = businesses[i].split('\t');
+    [lat, lon, name, locality] = row;
+    
+    lat = parseFloat(lat)
+    lon = parseFloat(lon)
+    
+    // errors
+    if(!maxBounds.contains([lat, lon])) {
+	console.log(row);
 	continue;
-  
-	if(lat < minLat) {				
-		minLat = lat;
-	}
-	
-	if(lat > maxLat) {			
-		maxLat = lat;
-	}
+    }
+    
+    if(lat < minLat) {				
+	minLat = lat;
+    }
+    
+    if(lat > maxLat) {			
+	maxLat = lat;
+    }
 
-	if(lon < minLon) {
-		minLon = lon;
-	}
+    if(lon < minLon) {
+	minLon = lon;
+    }
 
-	if(lon > maxLon ) {
-		maxLon = lon;
-	}
+    if(lon > maxLon ) {
+	maxLon = lon;
+    }
 
-  var circle;
-  if(neighborhoods.includes(locality)) {
-	  inboston++;
-	  circle = L.circle([lat, lon], {
-	  color: 'red',
-	  fillColor: 'red',
-	  fillOpacity: 1,
-	  radius: radius
-	  });
-  } else {
-	  not++
-	  circle = L.circle([lat, lon], {
-	  color: 'blue',
-	  fillColor: 'blue',
-	  fillOpacity: 1,
-	  radius: radius
-	  })
-  }
+    var circle;
+    if(neighborhoods.includes(locality)) {
+	inboston++;
+	circle = L.circle([lat, lon], {
+	    color: 'red',
+	    fillColor: 'red',
+	    fillOpacity: 1,
+	    radius: radius
+	});
+    } else {
+	not++
+	circle = L.circle([lat, lon], {
+	    color: 'blue',
+	    fillColor: 'blue',
+	    fillOpacity: 1,
+	    radius: radius
+	})
+    }
 
-	var l = L.latLng(lat, lon);
-	var dist = mymap.distance(boston, l) / 1609;
-	dist = Math.round(dist);
-  circle.bindPopup(`${name} - ${locality} (${dist}mi)`).openPopup();
-  
-  circle.addTo(mymap);
+    var l = L.latLng(lat, lon);
+    var dist = mymap.distance(boston, l) / 1609;
+    dist = Math.round(dist);
+    circle.bindPopup(`${name} - ${locality} (${dist}mi)`).openPopup();
+    
+    circle.addTo(mymap);
 }
 
 console.log([minLat, minLon], [maxLat, maxLon]);
 
 corner1 = L.latLng(minLat, minLon);
 corner2 = L.latLng(maxLat, maxLon);
-bounds = L.latLngBounds(corner1, corner2);
+var bounds = L.latLngBounds(corner1, corner2);
 
 mymap.fitBounds(bounds, { padding: [0,0] });
 
