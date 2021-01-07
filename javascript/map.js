a4455eb1db123142844420c2bb613a89f6b75174
jgwynn01 <joel.gwynn@tufts.edu>
2021-01-07 09:05:42 -0500

    removed fitbounds

--- javascript/map.js
+++ javascript/map.js
@@ -3,7 +3,7 @@
 var mymap = L.map('mapid').setView([42.247, -72], 8);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
@@ -11,7 +11,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: token
 }).addTo(mymap);
 
-var neighborhoods = ['Boston', 'Allston', 'Back Bay', 'Bay Village', 'Beacon Hill', 'Brighton', 'Charlestown', 'Chinatown–Leather District', 'Dorchester', 'Downtown', 'East Boston', 'Fenway-Kenmore (includes Longwood)', 'Hyde Park', 'Jamaica Plain', 'Mattapan', 'Mission Hill', 'North End', 'Roslindale', 'Roxbury', 'South Boston', 'South End', 'West End', 'West Roxbury', 'Wharf District']
+var neighborhoods = ['Boston', 'Allston', 'Back Bay', 'Bay Village', 'Beacon Hill', 'Brighton', 'Charlestown', 'Chinatownâ€“Leather District', 'Dorchester', 'Downtown', 'East Boston', 'Fenway-Kenmore (includes Longwood)', 'Hyde Park', 'Jamaica Plain', 'Mattapan', 'Mission Hill', 'North End', 'Roslindale', 'Roxbury', 'South Boston', 'South End', 'West End', 'West Roxbury', 'Wharf District']
 
 var boston = L.latLng(42.3601, -71.0589);
 
@@ -19,11 +19,6 @@ var length = businesses.length;
 var radius = 50;
 var inboston = 0;
 var not = 0;
-
-var minLat = 100;
-var maxLat = 0;
-var minLon = 0;
-var maxLon = -100;	  
 
 var corner1 = L.latLng(41, -74),
     corner2 = L.latLng(43, -70),
@@ -38,24 +33,8 @@ for(i = 0; i < length; i++) {
     
     // errors
     if(!maxBounds.contains([lat, lon])) {
-	console.log(row);
+	console.log(`out of bounds: ${row}`);
 	continue;
-    }
-    
-    if(lat < minLat) {				
-	minLat = lat;
-    }
-    
-    if(lat > maxLat) {			
-	maxLat = lat;
-    }
-
-    if(lon < minLon) {
-	minLon = lon;
-    }
-
-    if(lon > maxLon ) {
-	maxLon = lon;
     }
 
     var circle;
@@ -84,14 +63,6 @@ for(i = 0; i < length; i++) {
     
     circle.addTo(mymap);
 }
-
-console.log([minLat, minLon], [maxLat, maxLon]);
-
-corner1 = L.latLng(minLat, minLon);
-corner2 = L.latLng(maxLat, maxLon);
-var bounds = L.latLngBounds(corner1, corner2);
-
-mymap.fitBounds(bounds, { padding: [0,0] });
 
 document.getElementById('inboston').innerHTML = `${inboston} In Boston`;
 document.getElementById('not').innerHTML = `${not} Not`;
