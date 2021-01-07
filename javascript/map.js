29dabdf2b60fbacdd2df659483e5af6e621b35db
Joel Gwynn <joelman@gmail.com>
2021-01-07 15:08:40 -0500

    Added satellites

--- javascript/map.js
+++ javascript/map.js
@@ -11,9 +11,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: token
 }).addTo(mymap);
 
-var neighborhoods = ['Boston', 'Allston', 'Back Bay', 'Bay Village', 'Beacon Hill', 'Brighton', 'Charlestown', 'Chinatown', 'Dorchester', 'Dorchester Center', 
-'Downtown', 'East Boston', 'Fenway-Kenmore (includes Longwood)', 'Hyde Park', 'Jamaica Plain', 'Mattapan', 'Mission Hill', 'North End', 
-'Roslindale', 'Roxbury', 'Roxbury Crossing', 'South Boston', 'South End', 'West End', 'West Roxbury', 'Wharf District']
+var neighborhoods = ['Boston', 'Allston', 'Back Bay', 'Bay Village', 'Beacon Hill', 'Brighton', 'Charlestown', 'Chinatown',
+		     'Dorchester', 'Dorchester Center', 'Downtown', 'East Boston', 'Fenway-Kenmore (includes Longwood)',
+		     'Hyde Park', 'Jamaica Plain', 'Mattapan', 'Mission Hill', 'North End', 'Roslindale', 'Roxbury',
+		     'Roxbury Crossing', 'South Boston', 'South End', 'West End', 'West Roxbury', 'Wharf District']
 
 var boston = L.latLng(42.3601, -71.0589);
 
@@ -21,6 +22,9 @@ var length = businesses.length;
 var radius = 50;
 var inboston = 0;
 var not = 0;
+var satellite = 0;
+
+var points = [];
 
 var corner1 = L.latLng(41, -74),
     corner2 = L.latLng(43, -70),
@@ -39,33 +43,55 @@ for(i = 0; i < length; i++) {
 	continue;
     }
 
-    var circle;
-    if(neighborhoods.includes(locality)) {
-	inboston++;
-	circle = L.circle([lat, lon], {
-	    color: 'red',
-	    fillColor: 'red',
-	    fillOpacity: 1,
-	    radius: radius
-	});
-    } else {
-	not++
-	circle = L.circle([lat, lon], {
-	    color: 'blue',
-	    fillColor: 'blue',
-	    fillOpacity: 1,
-	    radius: radius
-	})
-    }
+    points.push({
+	lat: lat,
+	lon: lon,
+	name: name,
+	locality: locality
+    });
+}
 
-    var l = L.latLng(lat, lon);
+var bostonPoints = points.filter(x => x.locality == 'Boston');
+
+length = points.length;
+for(var i = 0; i < length; i++) {
+
+    var point = points[i]
+    
+    var color;
+    if(neighborhoods.includes(point.locality)) {
+	inboston++;
+	color = "red"
+    } else {
+	var sat = bostonPoints.filter(x => point.name == x.name);
+
+	if(sat.length) {
+//	    console.log(sat);
+	    satellite++;
+	    color = "purple";
+//	    console.log(`${point.name} satellite of ${sat[0].name}`);
+	} else {
+	    not++;
+	    color = "blue";
+	}
+    }
+    
+    var circle = L.circle([point.lat, point.lon], {
+	color: color,
+	fillColor: color,
+	fillOpacity: 1,
+	radius: radius
+    });
+
+    var l = L.latLng(point.lat, point.lon);
     var dist = mymap.distance(boston, l) / 1609;
     dist = Math.round(dist);
-    circle.bindPopup(`${name} - ${locality} (${dist}mi)`).openPopup();
+    circle.bindPopup(`${point.name} - ${point.locality} (${dist}mi)`).openPopup();
     
     circle.addTo(mymap);
 }
 
 document.getElementById('inboston').innerHTML = `${inboston} In Boston`;
 document.getElementById('not').innerHTML = `${not} Not`;
-document.getElementById('total').innerHTML = `${inboston + not} Total`;
+document.getElementById('satellite').innerHTML = `${satellite} Satellite`;
+document.getElementById('total').innerHTML = `${inboston + not + satellite} Total`;
